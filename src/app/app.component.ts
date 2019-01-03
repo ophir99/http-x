dd8251d90bb3d6c1f8f532c0c8150c61884745dc
kavya.pl <kavya.pl@gmail.com>
2019-01-03 21:51:03 +0530

    Simple commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,86 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { FormBuilder } from "@angular/forms";
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'http-x';
+  title = "http-x";
+  postForm;
+  editForm;
+  posts = [];
+  msg = "";
+  msg_ = "";
+  currentPostId;
+  url = "http://localhost:8900/posts";
+  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+    this.createForm();
+    this.createEditForm();
+    this.http.get(this.url).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.posts = res.posts;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  createForm() {
+    this.postForm = this.formBuilder.group({
+      title: [],
+      description: []
+    });
+  }
+  createEditForm() {
+    this.editForm = this.formBuilder.group({
+      title: [],
+      description: []
+    });
+  }
+
+  postData() {
+    this.msg = "Creating post.....";
+    this.http.post(this.url, this.postForm.value).subscribe(
+      res => {
+        this.msg = "Post created Successfully";
+      },
+      err => console.log(err)
+    );
+  }
+  editData() {
+    this.msg_ = "editing post.....";
+
+    // http://localhost:8900/posts/673263726372
+    this.http
+      .put(`${this.url}/${this.currentPostId}`, this.editForm.value)
+      .subscribe(
+        res => {
+          this.msg_ = "Post edit Successfully";
+        },
+        err => console.log(err)
+      );
+  }
+
+  setPost(post) {
+    this.currentPostId = post._id;
+    console.log(post);
+    this.editForm.patchValue(post);
+  }
+
+  deletePost(id) {
+    const obs = this.http.delete(`${this.url}/${id}`);
+
+    obs.subscribe(
+      res => {
+        console.log(res);
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
